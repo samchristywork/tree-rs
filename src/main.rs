1582eb3f9abda687a091e04b82373a7ca64dc13b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-06-24 15:52:16 -0500

    refactor: Include render module in main module
    Removing the rendering functions from the main module and using the
    versions from the render module instead.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+pub mod render;
 pub mod util;
 
+use crate::render::{print_tree, render};
 use crate::util::{filter_tree, print_node_name, term_setup, term_teardown};
 use clap::{arg, command, ArgGroup, Command};
 use crossterm::event::{self, Event, KeyCode};
@@ -410,61 +412,6 @@ fn read_dir_incremental(root: &mut TreeNode, dirname: PathBuf, limit: &mut i32) 
     }
 }
 
-fn print_tree(root: &TreeNode, indent: &Vec<String>, color_options: &ColorOptions) -> String {
-    let mut return_string = String::new();
-    let mut indent = indent.clone();
-
-    if indent.len() == 0 {
-        match color_options {
-            ColorOptions::Default => {
-                return_string.push_str(&format!("\x1b[{}m", root.color));
-                return_string.push_str(&format!("{}", root.val));
-                return_string.push_str(&format!("\x1b[0m\n"));
-            }
-            ColorOptions::NoColor => {
-                return_string.push_str(&format!("{}", root.val));
-                return_string.push_str(&format!("\n"));
-            }
-        }
-    } else {
-        match color_options {
-            ColorOptions::Default => {
-                return_string.push_str(&format!("{}──", indent.join("")));
-                return_string.push_str(&format!("\x1b[{}m", root.color));
-                return_string.push_str(&format!(" {}", root.val));
-                return_string.push_str(&format!("\x1b[0m\n"));
-            }
-            ColorOptions::NoColor => {
-                return_string.push_str(&format!("{}──", indent.join("")));
-                return_string.push_str(&format!(" {}", root.val));
-                return_string.push_str(&format!("\n"));
-            }
-        }
-    }
-
-    if root.children.len() != 0 {
-        if indent.len() > 0 && indent.last().unwrap() == "├" {
-            indent.pop();
-            indent.push("│   ".to_string());
-        }
-        if indent.len() > 0 && indent.last().unwrap() == "└" {
-            indent.pop();
-            indent.push("    ".to_string());
-        }
-        indent.push("├".to_string());
-    }
-
-    for (i, child) in root.children.iter().enumerate() {
-        if i == root.children.len() - 1 {
-            indent.pop();
-            indent.push("└".to_string());
-        }
-        return_string.push_str(&print_tree(child, &indent, color_options));
-    }
-
-    return_string
-}
-
 fn cli() -> Command {
     command!()
         .group(ArgGroup::new("LISTING OPTIONS").multiple(true))
@@ -600,64 +547,6 @@ async fn legacy_render(root: &mut TreeNode, dirname: PathBuf) {
                             refresh(&root, search_term.clone(), &mut terminal);
                             }
                         _ => {
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    term_teardown(&mut terminal);
-}
-
-fn render(root: &mut TreeNode, dirname: PathBuf) {
-    let mut terminal = term_setup();
-
-    let content = print_tree(&root, &Vec::new(), &ColorOptions::NoColor);
-    terminal.draw(|f| ui(f, None, Some(content))).unwrap();
-
-    let mut search_term = String::new();
-
-    let mut ret = None;
-    let mut running = true;
-    let mut duration = 0;
-    loop {
-        if running {
-            let mut counter = 0;
-            ret = legacy_read_dir_incremental(
-                root,
-                dirname.clone(),
-                ret,
-                &mut counter,
-                400,
-                &mut 0,
-                &mut 0,
-            );
-
-            if ret.is_none() {
-                running = false;
-                duration = 10;
-            }
-            refresh(&root, search_term.clone(), &mut terminal);
-        }
-
-        if let Ok(event) = event::poll(Duration::from_millis(duration)) {
-            if event {
-                if let Ok(event) = event::read() {
-                    if let Event::Key(key) = event {
-                        match key.code {
-                            KeyCode::Char(c) => {
-                                search_term.push(c);
-                                refresh(&root, search_term.clone(), &mut terminal);
-                            }
-                            KeyCode::Esc => {
-                                break;
-                            }
-                            KeyCode::Backspace => {
-                                search_term.pop();
-                                refresh(&root, search_term.clone(), &mut terminal);
-                            }
-                            _ => {}
                         }
                     }
                 }
